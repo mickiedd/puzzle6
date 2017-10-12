b0162c637086d0e78d6ef5e905cb8228ef2dfb28
mickiedd <mickiedd@gmail.com>
2017-10-12 20:16:34 +0800

    Update puzzle6_env.py

--- puzzle6/envs/puzzle6_env.py
+++ puzzle6/envs/puzzle6_env.py
@@ -1,1 +1,15 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
 
+class FooEnv(gym.Env):
+  metadata = {'render.modes': ['human']}
+
+  def __init__(self):
+    print("puzzle6 inited")
+  def _step(self, action):
+    print("step", action)
+  def _reset(self):
+    print("reset")
+  def _render(self, mode='human', close=False):
+    print("render")
