65331f5bcd5c31117fe1e5f4d07179335f54294e
root <mickiedd@gmail.com>
2017-10-16 19:33:31 +0800

    graphic support

--- puzzle6/envs/puzzle6_env.py
+++ puzzle6/envs/puzzle6_env.py
@@ -5,11 +5,28 @@ import os
 import sys
 import random
 from ctypes import *
+import math
+from gym.envs.classic_control import rendering
+
+def run(data):
+  # update the data
+  t, y = data
+  xdata.append(t)
+  ydata.append(y)
+  xmin, xmax = ax.get_xlim()
+
+  if t >= xmax:
+      ax.set_xlim(xmin, 2*xmax)
+      ax.figure.canvas.draw()
+  line.set_data(xdata, ydata)
+
+  return line,
 
 class Puzzle6Env(gym.Env):
   metadata = {'render.modes': ['human']}
-
+        
   def __init__(self):
+   
     print("puzzle6 inited")
     
     chessPropertiesTablePath = b"/home/mickie/course/puzzle6/data/LogicData/ChessPropertiesTable.bin"
@@ -38,6 +55,14 @@ class Puzzle6Env(gym.Env):
                                           spaces.Box(low=-180, high=180, shape=1),
                                           spaces.Box(low=0, high=100, shape=1),
                                           spaces.Box(low=-180, high=180, shape=1)))
+    
+    #graphic
+    self.viewer = None
+    # Angle at which to fail the episode
+    self.theta_threshold_radians = 12 * 2 * math.pi / 360
+    self.x_threshold = 2.4
+    self.state = None
+    
   def _step(self, action):
     print("step", action)
     
@@ -64,5 +89,37 @@ class Puzzle6Env(gym.Env):
     return ob, reward, self.episode_over, {}
   def _reset(self):
     print("reset")
+    
+  def get_chess(self):
+    cartwidth = 30.0
+    cartheight = 30.0
+    l,r,t,b = -cartwidth/2, cartwidth/2, cartheight/2, -cartheight/2
+    pole = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
+    pole.set_color(.8,.6,.4)
+    axleoffset =cartheight/4.0
+    self.poletrans = rendering.Transform(translation=(0, axleoffset))
+    pole.add_attr(self.poletrans)
+    self.viewer.add_geom(pole)
+    return pole
+    
   def _render(self, mode='human', close=False):
     print("render")
+    if close:
+        if self.viewer is not None:
+            self.viewer.close()
+            self.viewer = None
+            return
+    screen_width = 600
+    screen_height = 400
+
+    world_width = self.x_threshold*2
+    scale = screen_width/world_width
+    carty = 100 # TOP OF CART
+    polewidth = 10.0
+    polelen = scale * 1.0
+    
+    if self.viewer == None:
+        self.viewer = rendering.Viewer(screen_width, screen_height)
+        self.get_chess()
+    self.poletrans.set_translation(50, 25)
+    return self.viewer.render(return_rgb_array = mode=='rgb_array')
