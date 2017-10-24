dffc2299f436bda3efc826c03545a727d3c74cb4
root <mickiedd@gmail.com>
2017-10-24 22:30:17 +0800

    conv2d

--- puzzle6/test_sarsa.py
+++ puzzle6/test_sarsa.py
@@ -34,12 +34,12 @@ print(model.summary())
 # SARSA does not require a memory.
 policy = BoltzmannQPolicy()
 sarsa = SARSAAgent(model=model, nb_actions=nb_actions, nb_steps_warmup=10, policy=policy)
-sarsa.compile(Adam(lr=1e-3), metrics=['mae'])
+sarsa.compile(Adam(lr=1e-5), metrics=['mae'])
 
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-sarsa.fit(env, nb_steps=50000, visualize=False, verbose=2)
+sarsa.fit(env, nb_steps=50000000, visualize=False, verbose=2)
 
 # After training is done, we save the final weights.
 sarsa.save_weights('sarsa_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
